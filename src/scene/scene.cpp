77d541f0ea861351a504cf92f5d497e08cc664ae
Solar Flare <join200022@gmail.com>
2021-11-02 16:40:31 +0300

    Update scene
    Scene now connects all cells together during init

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -2,10 +2,10 @@
 #include <stdexcept>
 
 Scene::Scene(unsigned _rows, unsigned _cols) {
-    if (_rows < 1 || _rows > 64) {
+    if (_rows < 1 || _rows > getMaxSize()) {
         throw std::length_error("_rows");
     }
-    if (_cols < 1 || _cols > 64) {
+    if (_cols < 1 || _cols > getMaxSize()) {
         throw std::length_error("_rows");
     }
 
@@ -17,6 +17,24 @@ Scene::Scene(unsigned _rows, unsigned _cols) {
 
     rows = _rows;
     cols = _cols;
+
+    // Connect adjacent cells together
+    for (unsigned i = 0; i < _rows; i++) {
+        for (unsigned j = 0; j < _cols; j++) {
+            if (i != 0)
+                // Connect cell above
+                grid[i][j]->setNorthCell(grid[i - 1][j]);
+            if (i != _rows - 1)
+                // Connect cell below
+                grid[i][j]->setSouthCell(grid[i + 1][j]);
+            if (j != 0)
+                // Connect cell to the left
+                grid[i][j]->setWestCell(grid[i][j - 1]);
+            if (j != _cols - 1)
+                // Connect cell to the right
+                grid[i][j]->setEastCell(grid[i][j + 1]);
+        }
+    }
 }
 
 Scene::~Scene() {
@@ -25,4 +43,14 @@ Scene::~Scene() {
             delete grid[i][j];
         }
     }
+}
+
+const Scene::Grid& Scene::getGrid() const {
+    const Scene::Grid& ref = grid;
+    return ref;
+}
+
+Cell& Scene::getCell(unsigned row, unsigned col) const {
+    //TODO: Arg checks!
+    return *grid[row][col];
 }
--- src/scene/scene.hpp
+++ src/scene/scene.hpp
@@ -4,15 +4,22 @@
 #include <vector>
 
 class Scene {
+    using Grid = std::vector<std::vector<Cell*>>;
+
     public:
 	Scene(unsigned _rows, unsigned _cols);
         Scene(unsigned size) : Scene(size, size) {};
 
-        unsigned getRows() { return rows; }
-        unsigned getCols() { return cols; }
+        unsigned getRows() const { return rows; }
+        unsigned getCols() const { return cols; }
+        Cell& getCell(unsigned, unsigned) const;
+        const Grid& getGrid() const;
 
         ~Scene();
     private:
+        unsigned getMaxSize() const { return 24; }
+
         unsigned rows, cols;
-	std::vector<std::vector<Cell*>> grid;
+	    Grid grid;
 };
+
